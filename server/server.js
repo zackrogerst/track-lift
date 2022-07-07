982afdb3081fc5554a38150e70c84cab9772bb59
Zack Rogers <zack.rogers.t@gmail.com>
2022-07-07 12:24:29 -0600

    edited port

--- server/server.js
+++ server/server.js
@@ -93,4 +93,8 @@ function checkNotAuthenticated(req, res, next) {
     next()
 }
 
-app.listen(3000)
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
