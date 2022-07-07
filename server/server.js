aa3e1fe4b7d610b9a109e12970af35a8db5a1afb
Zack Rogers <zack.rogers.t@gmail.com>
2022-07-07 13:16:25 -0600

    updated endpoints

--- server/server.js
+++ server/server.js
@@ -40,13 +40,11 @@ app.use(methodOverride('_method'))
 
 
 app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name })
-    res.sendFile(path.join(__dirname, '../views/index.ejs'))
+    res.render('index.ejs', { name: req.user.name }).sendFile(path.join(__dirname, '../views/index.ejs'))
 })
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-    res.sendFile(path.join(__dirname, '../views/login.ejs'))
+    res.render('login.ejs').sendFile(path.join(__dirname, '../views/login.ejs'))
 })
 
 app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
@@ -56,8 +54,7 @@ app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
 }))
 
 app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/register.ejs'))
-    res.render('register.ejs')
+    res.render('register.ejs').sendFile(path.join(__dirname, '../views/register.ejs'))
 })
 
 app.post('/register', checkNotAuthenticated, async (req, res) => {
