a89b55fcb3f7ba4698c3b8f1233e38f37d27f6f6
Zack Rogers <zack.rogers.t@gmail.com>
2022-07-07 23:34:29 -0600

    formatted server

--- server/server.js
+++ server/server.js
@@ -2,18 +2,16 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
+const users = [] ////db?
+
 const express = require('express')
 const path = require('path')
-
 const app = express()
-
-
-const bcrypt = require('bcrypt')
-const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
 const methodOverride = require('method-override')
-
 const initializePassport = require('./passport-config')
 initializePassport(
     passport,
@@ -21,23 +19,16 @@ initializePassport(
     id => users.find(user => user.id === id)
 ) ///// db?
 
-const users = [] ////db?
-
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static("public"));
-
 app.use(flash())
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}))
-
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
+/////////////////// PAGES ///////////////////
 
 app.get('/', checkAuthenticated, (req, res) => {
     res.render('index.ejs', { name: req.user.name }, function (err, html) {
@@ -51,17 +42,19 @@ app.get('/login', checkNotAuthenticated, (req, res) => {
     })
 })
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-
 app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs', function (err, html) {
         res.send(html)
     })
 })
+
+/////////////////// USER ACCOUNT ///////////////////
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
 app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
@@ -88,6 +81,8 @@ app.delete('/logout', checkAuthenticated, function (req, res, next) {
     })
 })
 
+/////////////////// USER AUTH ///////////////////
+
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
@@ -102,8 +97,7 @@ function checkNotAuthenticated(req, res, next) {
     next()
 }
 
-const port = process.env.PORT || 3000
+/////////////////// LISTEN ///////////////////
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
